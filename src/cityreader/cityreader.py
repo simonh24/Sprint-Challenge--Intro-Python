285fc2ca442356219eac7ed9d48d42c83f686af3
unknown <simonh72498@gmail.com>
2020-08-07 11:56:42 -0400

    finished stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,15 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  def __str__(self):
+    return f"{self.name} | {self.lat} | {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -21,7 +30,12 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('c:/Users/Simon/Desktop/Lambda/Sprint-Challenge--Intro-Python/src/cityreader/cities.csv', newline = "") as csvfile:
+      c = csv.reader(csvfile, quotechar='|')
+      next(c)
+      for row in c:
+        new_city = City(row[0], float(row[3]), float(row[4]))
+        cities.append(new_city)
     return cities
 
 cityreader(cities)
@@ -63,7 +77,11 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
+  min_lat = min(lat1, lat2)
+  max_lat = max(lat1, lat2)
+  min_lon = min(lon1, lon2)
+  max_lon = max(lon1, lon2)
+  within = [i for i in cities if i.lat >= min_lat and i.lat <= max_lat and i.lon >= min_lon and i.lon <= max_lon]
   
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
